97a1af76491b712b91dda65f0c86855cb4b01096
Jacob Simionato <jsimionato@google.com>
2025-11-18 22:12:54 +1030

    Fix firebase options script

--- tool/stub_firebase_options.sh
+++ tool/stub_firebase_options.sh
@@ -6,13 +6,12 @@
 # Fast fail the script on failures.
 set -ex
 
-cp -f examples/travel_app/lib/firebase_options_stub.dart examples/travel_app/lib/firebase_options.dart
 cp -f examples/simple_chat/lib/firebase_options_stub.dart examples/simple_chat/lib/firebase_options.dart
-
 cp -f examples/simple_chat/macos/Runner/GoogleService-Info-stub.plist examples/simple_chat/macos/Runner/GoogleService-Info.plist
 cp -f examples/simple_chat/ios/Runner/GoogleService-Info-stub.plist examples/simple_chat/ios/Runner/GoogleService-Info.plist
 cp -f examples/simple_chat/android/app/google-services-stub.json examples/simple_chat/android/app/google-services.json
 
-cp -f examples/travel_app/macos/Runner/GoogleService-Info-stub.plist examples/simple_chat/macos/Runner/GoogleService-Info.plist
-cp -f examples/travel_app/ios/Runner/GoogleService-Info-stub.plist examples/simple_chat/ios/Runner/GoogleService-Info.plist
-cp -f examples/travel_app/android/app/google-services-stub.json examples/simple_chat/android/app/google-services.json
+cp -f examples/travel_app/lib/firebase_options_stub.dart examples/travel_app/lib/firebase_options.dart
+cp -f examples/travel_app/macos/Runner/GoogleService-Info-stub.plist examples/travel_app/macos/Runner/GoogleService-Info.plist
+cp -f examples/travel_app/ios/Runner/GoogleService-Info-stub.plist examples/travel_app/ios/Runner/GoogleService-Info.plist
+cp -f examples/travel_app/android/app/google-services-stub.json examples/travel_app/android/app/google-services.json
